99f401cc58516baad81c5f9278144a0f387227fc
bakarali <bakaralisunasra@gmail.com>
2016-04-21 20:42:38 +0530

    Added files via upload

--- stack.py
+++ stack.py
@@ -1,36 +1,35 @@
-l1=[]
-top = 0
 def push():
-	if len(l1)>5:
-		print("stack is full")	
-	else:			
-		l1.append(int(input("Enter element in stack")))
-def pop1():
-	if len(l1)<0:
-		print("stack is empty")	
-	else:	
-		l1.pop()
-def display():
-	print(l1)
-while(1):
-	print("1 : PUSH\n2 : POP\n3 : DISPLAY\n4 : EXIT")	
-	ch = int(input("Enter choice"))
-	if(ch==1):		
-		push()
-		
-	elif(ch==2):
-		pop1()
-
-	elif(ch==3):
-		display()
-	elif(ch==4):
-		break
+	if len(stack)>=max:
+		print("stackue is full")
 	else:
-		print(" Invalid choice")
+		stack.append(int(input("Enter element : ")))
 
-	
+def pop1():
+	if len(stack)==0:
+		print("stackue empty!!")
+	else:
+		stack.pop()
 
+def display():
+	if len(stack)==0:
+		print("stackue empty!!")
+	else:
+		print(stack)
 
-
-
+max = 5
+stack = []
+ch=0
+while (1):
+	print("1 : Insert \n2 : Delete \n3 : Display\n4 : Exit")
+	ch = int(input("Enter your choice"))
+	if ch == 1:
+		push()	
+	elif ch == 2:
+		pop1()
+	elif ch == 3:
+		display()
+	elif ch == 4:
+		break	
+	else:
+		printf("Invalid choice")	
 
